401dc10efb31bfe31ee67f83c93e57765d0822dd
Pebaz <pebazium@gmail.com>
2021-02-07 16:51:44 -0500

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+	name='dock-python-cli',
+	version='0.1.0',
+	author='https://github.com/Pebaz',
+	py_modules=['dock_cli'],
+	entry_points={
+		'console_scripts' : [
+			'dock=dock_cli:main'
+		]
+	}
+)
