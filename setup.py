701375e963c92a5098f0cfbb4024928b2481581f
Pebaz <pebazium@gmail.com>
2021-02-10 13:05:05 -0500

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name='dock-python-cli',
-	version='0.1.0',
+	version='0.1.1',
 	author='https://github.com/Pebaz',
 	py_modules=['dock_cli'],
 	entry_points={
